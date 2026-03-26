4c203295178ce55f0422618722963981fa80da22
someone <someone@someplace.com>
2026-03-26 17:08:38 +0800

    Добавлено хранилище с интерфейсом

--- cmd/main.go
+++ cmd/main.go
@@ -1,18 +1,18 @@
 package main
 
-import(
-	"fmt"
+import (
 	"flag"
+	"fmt"
 	"log"
 	"productStorage/internal/models"
 	"productStorage/internal/parser"
 	"productStorage/internal/storage"
 )
 
-func main(){
-	rep := storage.NewRepository[*models.Product]()
+func main() {
+	//rep := storage.NewRepository[*models.Product]()
 
-	fileNamePTR := flag.String("file", "data.txt", "")
+	fileNamePTR := flag.String("file", "data.txt", "Передайте адрес файла data файла")
 
 	flag.Parse()
 	// используется для того чтобы компилятор прочитал переданный флаг
@@ -22,23 +22,23 @@ func main(){
 	fmt.Printf("Система настроена на чтение из файла: %s", currentFile)
 	fmt.Printf("Чтение из файла (%s)", path)
 
-	Products, err := parser.ParseProductsFromFile(path)
+	var repos storage.productStorage = 
 
-	if err != nil{
-		log.Fatalf("Fatal Error: %v\n", err)
-	}
+	//if err != nil {
+	//	log.Fatalf("Fatal Error: %v\n", err)
+	//}
 
-	for _, product := range Products{
-		rep.Add(product)
-	}
+	// for _, product := range Products {
+	// 	rep.Add(product)
+	// }
 
-	items := rep.GetAll()
+	// items := rep.GetAll()
 
-	fmt.Printf("На склад отгружено %d товаров \n",len(items))
+	// fmt.Printf("На склад отгружено %d товаров \n", len(items))
 
-	for _, item := range items{
-		fmt.Printf("-Name: %10s | SBIN: %20s | Годен до %s \n", item.Name, item.SBIN, item.DateToString())
-	}
+	// for _, item := range items {
+	// 	fmt.Printf("-Name: %10s | SBIN: %20s | Годен до %s \n", item.Name, item.SBIN, item.DateToString())
+	// }
 
 	fmt.Scanln()
 }
---
+++ internal/storage/storage.go
@@ -0,0 +1,28 @@
+package storage
+
+import (
+	"productStorage/internal/models"
+)
+
+type ProductStorage interface{
+	Add(product *models.Product)
+	GetAll() []*models.Product
+}
+
+type Storage struct{
+	products []*models.Product
+}
+
+func NewStorage() ProductStorage{
+	return &Storage{
+		products: make([]*models.Product, 0),
+	}
+}
+
+func(storage *Storage) Add(product *models.Product){
+	storage.products = append(storage.products, product)
+}
+
+func(storage *Storage) GetAll() []*models.Product{
+	return storage.products
+}
